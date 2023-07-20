d8e3c7edfd0e9ff01255b2f6e4c9ee777cee1ac3
sahibpreettatras12 <sahibpreet.s@tatrasdata.com>
2023-07-21 02:03:02 +0530

    stories added

--- actions/actions.py
+++ actions/actions.py
@@ -9,6 +9,8 @@ import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
 from typing import Any, Text, Dict, List
 from dotenv import load_dotenv,find_dotenv
+from sentence_transformers import SentenceTransformer,util
+
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import UserUtteranceReverted, SlotSet,ActionExecuted,AllSlotsReset
@@ -58,7 +60,7 @@ vectordb.persist()
 
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 pdf_qa = ConversationalRetrievalChain.from_llm( OpenAI(temperature=0.0,) , 
-                                               vectordb.as_retriever(), memory=memory)
+                                               vectordb.as_retriever(search_kwargs={"k": 1}), memory=memory)
 query1 = """list down all the technologies on which the person has worked in their resume and give out response
  in python list"""
 result = pdf_qa({"question": query1})
@@ -92,7 +94,6 @@ def find_list_indices(string):
 
 result = get_completion(prompt=result,temperature=0.0)
 
-# print('Result 2 -->',result)
 
 indices = find_list_indices(result)
 print(indices)
@@ -101,11 +102,10 @@ print(indices)
 # then fetched whatever first list we found from result
 result = result[indices[0][0]:indices[0][1]+1]
 
-# if (result[0]=='[') and (result[-1]==']'):
-
 print('Before ast',result)
 result = ast.literal_eval(result)
 print('After ast',result)
+
 
 result = [element.lower() for element in result]
 print(result)
@@ -113,18 +113,66 @@ print(result)
 
 result = random.sample(result,5)
 
-propmt2 = f"""Given you are Artificial intelligent system now recommend 1 tough interview questions 
+
+# propmt2 = f"""Given you are Artificial intelligent system now recommend 1 tough interview questions 
+# for each element in {result} and return the output in the json format below
+
+# topic1 : [list of questions for topic1]
+
+# and so on
+
+# """
+
+propmt2 = f"""Given you are Artificial intelligent system now recommend 1 tough interview question and answer
 for each element in {result} and return the output in the json format below
 
-topic1 : [list of questions for topic1]
+topic1 : [list of questions for topic1,answer of questions for topic1]
 
 and so on
 
 """
+
+correct_answer_list = []
 answer_list=[]
 result = get_completion(prompt=propmt2,temperature=0.0)
 result = ast.literal_eval(result)
 print("result type is  -->",type(result))
+
+class ActionWelcomeOnBoard(Action):
+
+    def name(self) -> Text:
+        return "action_welcome_on_board"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+            global correct_answer_list
+
+            if len(correct_answer_list)==0:
+                buttons = []
+
+                    #append the response of API in the form of title and payload
+
+                buttons.append({"title": 'START QUIZ' , 'payload': '/start_qna'})
+
+                dispatcher.utter_message(text="""Welcome On board I am Interview Bot""")
+                dispatcher.utter_message(text="""I can help you get some Training on Topics of your RESUME""")
+                dispatcher.utter_message(text="""To get started""")
+                dispatcher.utter_message(text="""Hit on START or type in Let's start""",buttons=buttons)
+
+                return []
+            
+            else:
+                buttons = []
+
+        #append the response of API in the form of title and payload
+
+                buttons.append({"title": 'Continue Quiz' , 'payload': '/start_qna'})
+
+                dispatcher.utter_message(text='We are also now inculcating Video Response for Quiz',buttons=buttons)
+
+
+
 
 class ActionResultCheck(Action):
 
@@ -135,6 +183,7 @@ class ActionResultCheck(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         global answer_iter
+        model = SentenceTransformer('all-MiniLM-L6-v2')
         buttons = []
 
         #append the response of API in the form of title and payload
@@ -142,11 +191,27 @@ class ActionResultCheck(Action):
         buttons.append({"title": 'Next' , 'payload': '/start_qna'})
         
         if answer_iter >= 4:
-            print(answer_list)
-            dispatcher.utter_message(text='You have got good marks')
+
+            final_score=[]
+
+            #Compute embedding for both lists
+            embeddings1 = model.encode(answer_list, convert_to_tensor=False)
+            embeddings2 = model.encode(correct_answer_list, convert_to_tensor=False)
+
+
+            #Compute cosine-similarities
+            cosine_scores = util.cos_sim(embeddings1, embeddings2)
+
+            #Output the pairs with their score
+            for i in range(len(answer_list)):
+                k=cosine_scores[i][i]
+                final_score.append(k*2)
+
+            dispatcher.utter_message(text=f'You have {round(sum(final_score).item(),1)} out of 10')
             return []
         elif answer_iter ==0:
-            dispatcher.utter_message(text="Hi This is a hirebot happy to help")
+            dispatcher.utter_message(text="""Hi This is a hirebot happy to help. And to check your scores.
+                                            Please start the quiz    """)
             # dispatcher.utter_message(response='utter_result_check')
             
             p = tracker.get_slot('answer_1')
@@ -170,7 +235,7 @@ class ActionAskAnswer1(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         global answer_iter
-
+        global correct_answer_list
         if answer_iter==0:
             dispatcher.utter_message(text=f"We are Good Let's get educated together")
 
@@ -198,7 +263,22 @@ class ActionAskAnswer1(Action):
                 
 
                 #then display it using dispatcher
-                if len(j)>=1:
+                if len(j)>=2:
+                    answer_per_topic = j[1]
+                    # j = random.sample(j,1)
+                    j = j[:1]
+                    
+                    correct_answer_list.append(answer_per_topic)
+                    for question in j:
+
+                            dispatcher.utter_message(text=f"""For **{i}** you have """)
+                            dispatcher.utter_message(text=f"""1.  **{question}** """)
+
+                elif len(j)==1:
+                    answer_per_topic = 'Sorry we Have No answer'
+                    
+                    correct_answer_list.append(answer_per_topic)
+                    j = random.sample(j,1)
                     j = j[:1]
                     for question in j:
 
@@ -236,8 +316,13 @@ class ActionValidateAnswer1(Action):
             return [AllSlotsReset()]
         
         else:
+            buttons = []
+
+                #append the response of API in the form of title and payload
+
+            buttons.append({"title": 'Scores' , 'payload': '/result_check'})
             dispatcher.utter_message(text=f"""
-                                      You have submitted atleast 4 answers
-                                     \n so you can check scoes""")
+                                      You have submitted atleast 4 answers """)
+            dispatcher.utter_message(text = f"""so you can check scoes""",buttons=buttons)
             return []
 
